0c1fe9ea39022c0d604f621f5612b49a859e1b38
Yhonif <rumahkomposblk@gmail.com>
2025-07-12 14:09:31 +0700

    Add upload file module

--- src/pages/UploadFile.js
+++ src/pages/UploadFile.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Ganti dengan kredensial Supabase kamu
 const supabaseUrl = 'https://iepgyqsprvhwibwibkhu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // ANON PUBLIC KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcGd5cXNwcnZod2lid2lia2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyOTk1NDksImV4cCI6MjA2Nzg3NTU0OX0.1xEfdpkQKBPNHlzxJJJO6yntfQtXfvVGXVyBTzp3R68'; // ANON PUBLIC KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function UploadFile() {
