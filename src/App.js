8cbcf95c924c090bc226565d5bf730167a74c72c
Yhonif <rumahkomposblk@gmail.com>
2025-07-12 20:24:23 +0700

    Refactor dashboard admin layout

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
          <Route path="investor" element={<KelolaInvestor />} />
          <Route path="pengguna" element={<KelolaPengguna />} />
          <Route path="upload" element={<UploadFile />} />
-         <Route path="upload" element={<UploadFile />} /></Routes></AdminLayout>}/>
+         </Routes></AdminLayout>}/>
 
          </Route>
          </Route>
